02f8b97d0dd84b7d18d289cd227933ebe859732b
Gulu <gulusv19999@gmail.com>
2021-12-26 23:22:59 +0400

    ARCDECO TASK

---
+++ ARCDECO/assets/js/app.js
@@ -0,0 +1,31 @@
+$(function(){
+
+   $('.home-slider').owlCarousel({
+   items:1,
+   loop:true,
+   nav:true,
+   lazyLoad:true,
+   
+   responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            
+        },
+        600:{
+            items:1,
+            
+        },
+        1000:{
+            items:1,
+            
+            
+        }
+    }
+
+   });
+
+
+
+
+ });
